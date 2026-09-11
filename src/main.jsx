a359abb33362d27836d5d28dc6592fd4893defe4
Abhay jaiswal <72032951+Abhay123abhi@users.noreply.github.com>
2026-09-11 21:06:27 +0530

    feat: add full stack positioning and clean header styling

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import ThemeToggle from "./ThemeToggle";
+import PortfolioBranding from "./PortfolioBranding";
 import "./styles.css";
 import "./dark-theme.css";
+import "./header-refresh.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeToggle />
     <BrowserRouter>
+      <PortfolioBranding />
       <App />
     </BrowserRouter>
   </React.StrictMode>,
