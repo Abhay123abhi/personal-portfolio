6a7e48998a480800272870ce2d4400b5707ccd25
Abhay jaiswal <72032951+Abhay123abhi@users.noreply.github.com>
2026-09-21 17:44:19 +0530

    feat: make portfolio dark-only and load v2 styles

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import ThemeToggle from "./ThemeToggle";
 import "./styles.css";
 import "./header-refresh.css";
 import "./showcase-refresh.css";
@@ -10,10 +9,12 @@ import "./theme.css";
 import "./motion.css";
 import "./design-overhaul.css";
 import "./responsive.css";
+import "./v2/v2.css";
+
+document.documentElement.dataset.theme = "dark";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ThemeToggle />
     <BrowserRouter>
       <App />
     </BrowserRouter>
