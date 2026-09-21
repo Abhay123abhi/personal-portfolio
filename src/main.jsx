ead1007ff12246ab06788ba226fdd87b1a664872
Abhay jaiswal <72032951+Abhay123abhi@users.noreply.github.com>
2026-09-21 22:48:13 +0530

    cleanup: remove retired showcase stylesheet import

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./styles.css";
 import "./header-refresh.css";
-import "./showcase-refresh.css";
 import "./theme.css";
 import "./motion.css";
 import "./design-overhaul.css";
